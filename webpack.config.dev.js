4afc70bad36eacfb3b1abede94778f1c9bd937c6
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-05-25 19:33:52 -0500

    Project initial configuration

---
+++ webpack.config.dev.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ],
+        // alias: {
+
+        // }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist/')
+        },
+        compress: true,
+        port: 3005
+    }
+}
